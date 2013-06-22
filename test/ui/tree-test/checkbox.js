43cb786ae5f49c608f5b5783402fa5099150a1d1
Kris Kowal <kris.kowal@cixar.com>
2013-06-21 20:00:07 -0700

    Fix checkbox stub in tree controller test
    Now updates the underlying element's checked and disabled properties.

--- test/ui/tree-test/checkbox.js
+++ test/ui/tree-test/checkbox.js
@@ -5,6 +5,40 @@ exports.Checkbox = AbstractCheckbox.specialize({
 
     hasTemplate: {value: false},
 
+    checked: {
+        get: function () {
+            return this._checked;
+        },
+        set: function (checked) {
+            if (checked === this._checked)
+                return;
+            this._checked = checked;
+            this.needsDraw = true;
+            this.dispatchOwnPropertyChange("checked", checked);
+        }
+    },
+
+    enabled: {
+        get: function () {
+            return this._enabled;
+        },
+        set: function (enabled) {
+            if (enabled === this._enabled)
+                return;
+            this._enabled = enabled;
+            this.needsDraw = true;
+            this.dispatchOwnPropertyChange("enabled", enabled);
+        }
+    },
+
+    draw: {
+        value: function () {
+            this.super();
+            this.element.checked = this._checked;
+            this.element.disabled = !this.enabled;
+        }
+    },
+
     enterDocument: {
         value: function(firstTime) {
             if (firstTime) {
